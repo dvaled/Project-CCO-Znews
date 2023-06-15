d59cbd49806773de33929ad350230b589278908f
dvaled <davaalif0@gmail.com>
2023-06-15 22:58:54 +0700

    first commit

---
+++ asset/js/tambah_berita.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB28tU_FYy3nfmZ2sBLkn2AC6mt3Z3p1NM",
+    authDomain: "test1-b539f.firebaseapp.com",
+    databaseURL: "https://test1-b539f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "test1-b539f",
+    storageBucket: "test1-b539f.appspot.com",
+    messagingSenderId: "489262907651",
+    appId: "1:489262907651:web:20cbde456a597ab31ae936"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
